6f5ee0c77b8e1ff01a06597c804d593493d8b686
luisgaleas-clockwork <luis.galeasjr@gmail.com>
2020-07-10 17:31:34 -0400

    added Components and Pages

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+    return(
+        <div id="heading">
+            <h1>Fine Po!nt</h1>
+            <p>Creating responive webpage applications at it's finest one point at a time.</p>
+            <button>Learn More</button>
+        </div>
+    )
+}
+
+export default Header;
